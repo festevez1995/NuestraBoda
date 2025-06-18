d5731e6d4cd47869af663a07bf28f90c3d4b00a5
festevez1995 <estevezfernando1995@gmail.com>
2025-06-17 23:20:30 -0700

    Adjusted color names?

--- src/components/AttireBody.jsx
+++ src/components/AttireBody.jsx
@@ -7,15 +7,15 @@ function ReceptionBody() {
     <div className="attire-container">
       <h3>Código De Vestimenta</h3>
       <p style={{ color: "#000000" }}>
-        Para quienes quieran combinar con el tema de nuestra boda, estos son los
+        Si les gustaria combinar con el tema de nuestra boda, estos son los
         colores. Este código de vestimenta no es obligatorio.
       </p>
       <br />
       <div className="colorCard-container">
-        <NewCard color="#800020" colorName="Burgundy" />
-        <NewCard color="#efbf04" colorName="Gold" />
-        <NewCard color="#C4A484" colorName="Light Brown" />
-        <NewCard color="#06402b" colorName="Dark Green" />
+        <NewCard color="#800020" colorName="Burgundy/Vino" />
+        <NewCard color="#efbf04" colorName="Gold/Oro" />
+        <NewCard color="#C4A484" colorName="Brown/Marron" />
+        <NewCard color="#06402b" colorName="Dark Green/Verde Oscuro" />
       </div>
     </div>
   );
